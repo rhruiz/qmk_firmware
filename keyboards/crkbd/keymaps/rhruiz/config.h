a2bbe135847bff885d91df80d2e339b8bb95e5d6
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2020-04-16 22:51:43 -0300

    add oled scroll timeout

--- keyboards/crkbd/keymaps/rhruiz/config.h
+++ keyboards/crkbd/keymaps/rhruiz/config.h
@@ -28,7 +28,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifdef OLED_DRIVER_ENABLE
 #    undef SSD1306OLED
+#    ifdef OLED_FONT_H
+#        undef OLED_FONT_H
+#    endif
 #    define OLED_FONT_H "users/rhruiz/glcdfont.c"
+#    define OLED_SCROLL_TIMEOUT 8000
 #endif
 
 #undef USE_I2C
