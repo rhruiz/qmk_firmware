fe6bed484accd65c30eef73db8d21fb1c050fd1f
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2021-09-04 12:41:11 -0300

    Change home row mods to a macro function

--- users/rhruiz/layouts/wrappers.h
+++ users/rhruiz/layouts/wrappers.h
@@ -14,17 +14,15 @@
 #define KC__F10 KC_F10
 
 #ifdef HOME_ROW_MODS
-#    define KC_GF LGUI_T(KC_F)
-#    define KC_GJ LGUI_T(KC_J)
-
-#    define KC_SS LSFT_T(KC_S)
-#    define KC_SL LSFT_T(KC_L)
-
-#    define KC_AA LALT_T(KC_A)
-#    define KC_ASCL LALT_T(KC_SCLN)
-
-#    define KC_CD LCTL_T(KC_D)
-#    define KC_CK LCTL_T(KC_K)
+#    define KC__LALT_T(k) LALT_T(k)
+#    define KC__LSFT_T(k) LSFT_T(k)
+#    define KC__LCTL_T(k) LCTL_T(k)
+#    define KC__LGUI_T(k) LGUI_T(k)
+#    define HRL(k1, k2, k3, k4, k5) _LALT_T(KC_##k1), _LSFT_T(KC_##k2), _LCTL_T(KC_##k3), _LGUI_T(KC_##k4), k5
+#    define HRR(k1, k2, k3, k4, k5) k1, _LGUI_T(KC_##k2), _LCTL_T(KC_##k3), _LSFT_T(KC_##k4), _LALT_T(KC_##k5)
+#else
+#    define HRL(k1, k2, k3, k4, k5) k1, k2, k3, k4, k5
+#    define HRR(k1, k2, k3, k4, k5) k1, k2, k3, k4, k5
 #endif
 
 // clang-format off
@@ -42,91 +40,83 @@
 #define _____SYMBOLS_RGHT_____ CIRC,AMPR,ASTR,LPRN,RPRN
 //                            `----+----+----+----+----'
 
-//                             .------------------------.
-#define ________L_BL_R1________  _____NUMBERS_LEFT_____
-//                             |----+----+----+----+----|
-#define ________L_BL_R2________   Q ,  W ,  E ,  R ,  T
-//                             |----+----+----+----+----|
-#ifdef HOME_ROW_MODS        // |    |    |    |    |    |
-#define ________L_BL_R3________  AA , SS , CD , GF ,  G
-#else                       // |    |    |    |    |    |
-#define ________L_BL_R3________   A ,  S ,  D ,  F ,  G
-#endif                      // |    |    |    |    |    |
-//                             |----+----+----+----+----|
-#define ________L_BL_R4________   Z ,  X ,  C ,  V ,  B
-//                             `----+----+----+----+----'
+//                                 .------------------------.
+#define ________L_BL_R1________      _____NUMBERS_LEFT_____
+//                                 |----+----+----+----+----|
+#define ________L_BL_R2________       Q ,  W ,  E ,  R ,  T
+//                                 |----+----+----+----+----|
+#define ________L_BL_R3________ HRL(  A ,  S ,  D ,  F ,  G )
+//                                 |----+----+----+----+----|
+#define ________L_BL_R4________       Z ,  X ,  C ,  V ,  B
+//                                 `----+----+----+----+----'
 
-//                             .------------------------.
-#define ________R_BL_R1________  _____NUMBERS_RGHT_____
-//                             |----+----+----+----+----|
-#define ________R_BL_R2________   Y ,  U ,  I ,  O ,  P
-//                             |----+----+----+----+----|
-#ifdef HOME_ROW_MODS        // |    |    |    |    |    |
-#define ________R_BL_R3________   H , GJ , CK , SL ,ASCL
-#else                       // |    |    |    |    |    |
-#define ________R_BL_R3________   H ,  J ,  K ,  L ,SCLN
-#endif                      // |    |    |    |    |    |
-//                             |----+----+----+----+----|
-#define ________R_BL_R4________   N ,  M ,COMM, DOT,SLSH
-//                             `----+----+----+----+----'
+//                                 .------------------------.
+#define ________R_BL_R1________      _____NUMBERS_RGHT_____
+//                                 |----+----+----+----+----|
+#define ________R_BL_R2________       Y ,  U ,  I ,  O ,  P
+//                                 |----+----+----+----+----|
+#define ________R_BL_R3________ HRR(  H ,  J ,  K ,  L ,SCLN)
+//                                 |----+----+----+----+----|
+#define ________R_BL_R4________       N ,  M ,COMM, DOT,SLSH
+//                                 `----+----+----+----+----'
 
-//                             .------------------------.
-#define ________L_LWR_1________  ____FUNCTIONS_LEFT____
-//                             |----+----+----+----+----|
-#define ________L_LWR_2________ EXLM,LCBR,RCBR, DLR,PERC
-//                             |----+----+----+----+----|
-#define ________L_LWR_3________ HASH,LBRC,RBRC,MINS,UNDS
-//                             |----+----+----+----+----|
-#define ________L_LWR_4________  AT ,LPRN,RPRN, EQL,PLUS
-//                             `----+----+----+----+----'
+//                                 .------------------------.
+#define ________L_LWR_1________      ____FUNCTIONS_LEFT____
+//                                 |----+----+----+----+----|
+#define ________L_LWR_2________     EXLM,LCBR,RCBR, DLR,PERC
+//                                 |----+----+----+----+----|
+#define ________L_LWR_3________     HASH,LBRC,RBRC,MINS,UNDS
+//                                 |----+----+----+----+----|
+#define ________L_LWR_4________      AT ,LPRN,RPRN, EQL,PLUS
+//                                 `----+----+----+----+----'
 
-//                             .------------------------.
-#define ________R_LWR_1________  ____FUNCTIONS_RGHT____
-//                             |----+----+----+----+----|
-#define ________R_LWR_2________     ,EPIP,ASTR,BSLS,
-//                             |----+----+----+----+----|
-#define ________R_LWR_3________  GRV,AMPR,PIPE,CIRC,QUOT
-//                             |----+----+----+----+----|
-#define ________R_LWR_4________     ,    ,TILD, GRV,QUES
-//                             `----+----+----+----+----'
+//                                 .------------------------.
+#define ________R_LWR_1________      ____FUNCTIONS_RGHT____
+//                                 |----+----+----+----+----|
+#define ________R_LWR_2________         ,EPIP,ASTR,BSLS,
+//                                 |----+----+----+----+----|
+#define ________R_LWR_3________      GRV,AMPR,PIPE,CIRC,QUOT
+//                                 |----+----+----+----+----|
+#define ________R_LWR_4________         ,    ,TILD, GRV,QUES
+//                                 `----+----+----+----+----'
 
-//                             .------------------------.
-#define ________L_RSE_1________ SLCK,PAUS,MISS,    ,
-//                             |----+----+----+----+----|
-#define ________L_RSE_2________  _____NUMBERS_LEFT_____
-//                             |----+----+----+----+----|
-#define ________L_RSE_3________  _WN,_TAP,_TAN, TAB,
-//                             |----+----+----+----+----|
-#define ________L_RSE_4________ CTAB,_BCK,_FWD, ESC,
-//                             `----+----+----+----+----'
+//                                 .------------------------.
+#define ________L_RSE_1________     SLCK,PAUS,MISS,    ,
+//                                 |----+----+----+----+----|
+#define ________L_RSE_2________      _____NUMBERS_LEFT_____
+//                                 |----+----+----+----+----|
+#define ________L_RSE_3________     _WN,_TAP,_TAN, TAB,
+//                                 |----+----+----+----+----|
+#define ________L_RSE_4________     CTAB,_BCK,_FWD, ESC,
+//                                 `----+----+----+----+----'
 
-//                             .------------------------.
-#define ________R_RSE_1________  _____SYMBOLS_RGHT_____
-//                             |----+----+----+----+----|
-#define ________R_RSE_2________  _____NUMBERS_RGHT_____
-//                             |----+----+----+----+----|
-#define ________R_RSE_3________ PLUS,LEFT,DOWN, UP ,RGHT
-//                             |----+----+----+----+----|
-#define ________R_RSE_4________ MINS,ASTR,COMM, DOT,SLSH
-//                             `----+----+----+----+----'
+//                                 .------------------------.
+#define ________R_RSE_1________      _____SYMBOLS_RGHT_____
+//                                 |----+----+----+----+----|
+#define ________R_RSE_2________      _____NUMBERS_RGHT_____
+//                                 |----+----+----+----+----|
+#define ________R_RSE_3________     PLUS,LEFT,DOWN, UP ,RGHT
+//                                 |----+----+----+----+----|
+#define ________R_RSE_4________     MINS,ASTR,COMM, DOT,SLSH
+//                                 `----+----+----+----+----'
 
-//                             .----+----+----+----+----.
-#define ________L_CFG_1________ SLCK,PAUS,MISS,RVAD,RVAI
-//                             |----+----+----+----+----|
-#define ________L_CFG_2________ RTOG,RMOD,RSAI,RVAI,RHUI
-//                             |----+----+----+----+----|
-#define ________L_CFG_3________     ,    ,    ,    ,
-//                             |----+----+----+----+----|
-#define ________L_CFG_4________ _VUP,_VDN,MUTE,EJCT,
-//                             `----+----+----+----+----'
+//                                 .----+----+----+----+----.
+#define ________L_CFG_1________     SLCK,PAUS,MISS,RVAD,RVAI
+//                                 |----+----+----+----+----|
+#define ________L_CFG_2________     RTOG,RMOD,RSAI,RVAI,RHUI
+//                                 |----+----+----+----+----|
+#define ________L_CFG_3________         ,    ,    ,    ,
+//                                 |----+----+----+----+----|
+#define ________L_CFG_4________     _VUP,_VDN,MUTE,EJCT,
+//                                 `----+----+----+----+----'
 
-//                             .----+----+----+----+----.
-#define ________R_CFG_1________     ,    ,    ,    ,
-//                             |----+----+----+----+----|
-#define ________R_CFG_2________     ,_BOL,_SBT,_STP,_EOL
-//                             |----+----+----+----+----|
-#define ________R_CFG_3________     , _WL,PGDN,PGUP, _WR
-//                             |----+----+----+----+----|
-#define ________R_CFG_4________     ,    ,MRWD,MFFD,MPLY
-//                             `----+----+----+----+----'
+//                                 .----+----+----+----+----.
+#define ________R_CFG_1________         ,    ,    ,    ,
+//                                 |----+----+----+----+----|
+#define ________R_CFG_2________         ,_BOL,_SBT,_STP,_EOL
+//                                 |----+----+----+----+----|
+#define ________R_CFG_3________         , _WL,PGDN,PGUP, _WR
+//                                 |----+----+----+----+----|
+#define ________R_CFG_4________         ,    ,MRWD,MFFD,MPLY
+//                                 `----+----+----+----+----'
 // clang-format on
