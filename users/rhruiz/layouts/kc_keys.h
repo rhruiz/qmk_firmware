03c6f2597187ed1aa62e537fd0c7804ac26d7011
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2022-04-24 11:49:53 -0300

    Begin removing layout_kc (shark, crkbd)

--- users/rhruiz/layouts/kc_keys.h
+++ users/rhruiz/layouts/kc_keys.h
@@ -1,37 +1,47 @@
 #include "quantum.h"
 
-#define KC_ KC_TRNS
+#define CTL_SPC LCTL_T(KC_SPACE)
+#define ALT_BS RALT_T(KC_BSPC)
+#define ALT_DEL LALT_T(KC_DEL)
+
+#define MO_LWR MO(_FN1)
+#define MO_RSE MO(_FN2)
+#define MO_NUM MO(_NUM)
+#define MO_CFG MO(_CFG)
+#define OSL_GLWR OSL(_GAMEFN1)
+#define TG_NUM TG(_NUM)
+#define TG_GAME TG(_GAME)
+#define SFT_SPC LSFT_T(KC_SPACE)
+#define SFT_ENT LSFT_T(KC_ENTER)
+#define STAB LSFT(KC_TAB)
+
+#define NXT_WIN LCMD(KC_GRV)
+#define KC__STP LCMD(KC_UP)
+#define KC__SBT LCMD(KC_DOWN)
+#define KC__EOL LCMD(KC_RIGHT)
+#define KC__BOL LCMD(KC_LEFT)
+#define KC__WL LALT(KC_LEFT)
+#define KC__WR LALT(KC_RIGHT)
+#define KC__BCK LCMD(KC_LBRC)
+#define KC__FWD LCMD(KC_RBRC)
+#define KC__TAN LCMD(KC_RCBR)
+#define KC__TAP LCMD(KC_LCBR)
+#define KC__NXW LCMD(KC_TAB)
+
+
 #define KC_XXX KC_NO
-
-#define KC_CESC CTRLESC
-#define KC__CSP LCTL_T(KC_SPACE)
-
 #define KC__DLO DF(_BL)
 #define KC__ALO DF(_ALT_BL)
-
-#define KC_OFN1 OSL(_FN1)
 #define KC_OFN2 OSL(_FN2)
-#define KC__FN1 MO(_FN1)
-#define KC__FN2 MO(_FN2)
 #define KC_AFN1 MO(_ALT_FN1)
 #define KC_AFN2 MO(_ALT_FN2)
-#define KC__CFG MO(_CFG)
-#define KC_FNUM MO(_NUM)
-#define KC__TGN TG(_NUM)
+
 #define KC_TGAN TG(_ALT_NUM)
-#define KC__TGM TG(_GAME)
 #define KC_SFN1 SPCFN1
 #define KC_BFN2 BKSFN2
 #define KC_EFN2 LT(_FN2, KC_ENTER)
 #define KC_TFN2 LT(_FN2, KC_TAB)
-#define KC_ALBS RALT_T(KC_BSPC)
-#define KC_ALDE LALT_T(KC_DEL)
-#define KC__GFN1 OSL(_GAMEFN1)
-#define KC_ESPC LSFT_T(KC_SPACE)
-#define KC_STAB LSFT(KC_TAB)
 
-#define KC__RST QK_BOOT
-#define KC_MISS MISCTRL
 #define KC__VUP KC__VOLUP
 #define KC__VDN KC__VOLDOWN
 
@@ -45,39 +55,4 @@
 #    define KC__CDN KC_TRNS
 #    define KC__CTG KC_TRNS
 #    define KC_MUTG KC_TRNS
-#endif
-
-#define KC_ENTS LSFT_T(KC_ENTER)
-
-#define KC__WN LCMD(KC_GRV)
-#define KC__STP LCMD(KC_UP)
-#define KC__SBT LCMD(KC_DOWN)
-#define KC__EOL LCMD(KC_RIGHT)
-#define KC__BOL LCMD(KC_LEFT)
-#define KC__WL LALT(KC_LEFT)
-#define KC__WR LALT(KC_RIGHT)
-#define KC__BCK LCMD(KC_LBRC)
-#define KC__FWD LCMD(KC_RBRC)
-#define KC__TAN LCMD(KC_RCBR)
-#define KC__TAP LCMD(KC_LCBR)
-#define KC__NXW LCMD(KC_TAB)
-
-#ifdef RGBLIGHT_ENABLE
-#    define KC_RVAD RGB_VAD
-#    define KC_RVAI RGB_VAI
-#    define KC_RTOG RGB_TOG
-#    define KC_RMOD RGB_MOD
-#    define KC_RHUI RGB_HUI
-#    define KC_RHUD RGB_HUD
-#    define KC_RSAI RGB_SAI
-#    define KC_RSAD RGB_SAD
-#else
-#    define KC_RVAD KC_NO
-#    define KC_RVAI KC_NO
-#    define KC_RTOG KC_NO
-#    define KC_RMOD KC_NO
-#    define KC_RHUI KC_NO
-#    define KC_RHUD KC_NO
-#    define KC_RSAI KC_NO
-#    define KC_RSAD KC_NO
 #endif
