d6763ecda556c64d559844ce369cb85661686965
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2022-04-30 16:21:54 -0300

    Rename my keys (dz60)

--- keyboards/dz60/keymaps/rhruiz/keymap.c
+++ keyboards/dz60/keymaps/rhruiz/keymap.c
@@ -34,14 +34,20 @@ const hue_sat_pair hue_sat_pairs[][2] = {[_FN1] = {[false] = {2, 255}, [true] = 
 // clang-format off
 
 #define KC_CSPC LCTL_T(KC_SPACE)
+#define CLLEFT MT(MOD_RGUI, KC_LEFT)
+#define CLDOWN MT(MOD_RCTL, KC_DOWN)
+#define CLUP MT(MOD_RSFT, KC_UP)
+#define CLRIGHT LT(_NUM, KC_RIGHT)
+
+#define LAYOUT_wrapper(...) LAYOUT(__VA_ARGS__)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_BL] = LAYOUT(
-		KC_GRV,    KC_1,     KC_2,     KC_3,    KC_4,      KC_5,    KC_6,    KC_7,    KC_8,   KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSLS,   KC_PIPE,
-		KC_TAB,    KC_Q,     KC_W,     KC_E,    KC_R,      KC_T,    KC_Y,    KC_U,    KC_I,   KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSPC,
-		CTRLESC,   KC_A,     KC_S,     KC_D,    KC_F,      KC_G,    KC_H,    KC_J,    KC_K,   KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,
-		KC_LSFT,   KC_NO,    KC_Z,     KC_X,    KC_C,      KC_V,    KC_B,    KC_N,    KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  CLUP,     KC__FN2,
-		KC__FN1,   KC_LALT,  KC_LGUI,  KC_CSPC, KC__FN2,   BKSFN1,  KC_RALT, CLLEFT,  KC_NO,  CLDOWN,   CLRIGHT
+  [_BL] = LAYOUT_wrapper(
+		KC_GRV ,   ________L_BL_R1________, ________R_BL_R1________,  KC_MINS,  KC_EQL,   KC_BSLS,   KC_PIPE,
+		KC_TAB ,   ________L_BL_R2________, ________R_BL_R2________,  KC_LBRC,  KC_RBRC,  KC_BSPC,
+		CT_ESC ,   ________L_BL_R3________, ________R_BL_R3________,  KC_QUOT,  KC_ENT,
+		KC_LSFT,   KC_NO,    ________L_BL_R4________, ________R_BL_R4________,  CLUP,     MO_RSE ,
+		MO_LWR ,   KC_LALT,  KC_LGUI,  CT_SPC , MO_LWR ,  LT_RSE_ENT,  AT_BS  , CLLEFT,  KC_NO,  CLDOWN,   CLRIGHT
   ),
 
   [_NUM] = LAYOUT(
@@ -60,16 +66,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______,  _______,  _______,  _______,  _______,  _DELFN1,  _______,  _______,  _______,  _______,  _______
   ),
 
-  [_FN1] = LAYOUT(
-		_______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,     KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,   _______,
-		_______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-		_______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-		_______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+  [_FN1] = LAYOUT_wrapper(
+		_______,    ________L_LWR_1________  ,     ________R_LWR_1________  ,  KC_F11 ,  KC_F12 ,  _______,  _______,
+		_______,    ________L_LWR_2________  ,     ________R_LWR_2________  ,  _______,  _______,  _______,
+		_______,    ________L_LWR_3________  ,     ________R_LWR_3________  ,  _______,  _______,
+		_______,  _______,    ________L_LWR_4________  ,     ________R_LWR_3________  ,  _______,  _______,
 		_______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______
   ),
 
   [_FN2] = LAYOUT(
-		_______,   KC_SLCK,  KC_PAUS,  MISCTRL,  _______,  RGB_VAD,   RGB_VAI,  KC_MRWD,  KC_MPLY,  KC_MFFD,  KC_MUTE,  KC__VOLDOWN,  KC__VOLUP,  _______,  _______,
+		_______,   KC_SLCK,  KC_PAUS,  NV_MICT,  _______,  RGB_VAD,   RGB_VAI,  KC_MRWD,  KC_MPLY,  KC_MFFD,  KC_MUTE,  KC__VOLDOWN,  KC__VOLUP,  _______,  _______,
 		_______,   KC_PIPE,  KC_LCBR,  KC_RCBR,  KC_UNDS,  KC_PLUS,   _______,  _______,  _______,  _______,  _______,  _______,      _______,    _______,
 		_______,   KC_BSLS,  KC_LBRC,  KC_RBRC,  KC_MINS,  KC_EQL ,   _______,  _______,  _______,  _______,  _______,  _______,      _______,
 		_______,   _______,  KC_EPIP,  KC_LPRN,  KC_RPRN,  _______,   _______,  _______,  _______,  _______,  _______,  _______,      _______,    _______,
@@ -78,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_CFG] = LAYOUT(
 		_______,  RGB_M_P,    RGB_M_B,      RGB_M_R,  RGB_M_SW,  RGB_M_SN,  RGB_M_K,  RGB_M_X,  RGB_M_G,  RGB_M_T,  _______,  RGB_SPD,  RGB_SPI,  KC_MAKE,  KC_KBVSN,
-		_______,  RGB_TOG,    RGB_MOD,      RGB_HUI,  RGB_HUD,   RGB_SAI,   RGB_SAD,  RGB_VAI,  RGB_VAD,  _______,  _______,  _______,  _______,  RESET,
+		_______,  RGB_TOG,    RGB_MOD,      RGB_HUI,  RGB_HUD,   RGB_SAI,   RGB_SAD,  RGB_VAI,  RGB_VAD,  _______,  _______,  _______,  _______,  QK_BOOT,
 		_______,  KC__VOLUP,  KC__VOLDOWN,  KC_MUTE,  KC_EJCT,   _______,   KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   _______,  _______,  _______,
 		_______,  _______,    BL_TOGG,      BL_STEP,  BL_BRTG,   _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
 		_______,  _______,    _______,      _______,  _______,   _______,   _______,  _______,  _______,  _______,  _______
