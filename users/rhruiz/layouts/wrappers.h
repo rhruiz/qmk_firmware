1a9a80da15651ae244029ee2b0f89e6b1782ca16
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2022-09-16 21:52:10 -0300

    More symbols shuffle

--- users/rhruiz/layouts/wrappers.h
+++ users/rhruiz/layouts/wrappers.h
@@ -180,11 +180,11 @@
 //                                 .--------------------------------------------.
 #define ________R_LWR_1________                 ____FUNCTIONS_RGHT____
 //                                 |--------+--------+--------+--------+--------|
-#define ________R_LWR_2________      KC_PDIR, KC_EPIP, KC_CIRC, KC_PIPE, KC_BSLS
+#define ________R_LWR_2________      KC_PDIR, KC_EPIP, KC_CIRC, KC_AMPR, _______
 //                                 |--------+--------+--------+--------+--------|
-#define ________R_LWR_3________      KC_GRV , KC_COLN, KC_ASTR, KC_AMPR, KC_QUOT
+#define ________R_LWR_3________      KC_GRV , KC_COLN, KC_ASTR, KC_PIPE, KC_QUOT
 //                                 |--------+--------+--------+--------+--------|
-#define ________R_LWR_4________      KC_TILD, KC_SCLN, KC_ARRW, KC_FARW, _______
+#define ________R_LWR_4________      KC_TILD, KC_SCLN, KC_ARRW, KC_FARW, KC_BSLS
 //                                 `--------+--------+--------+--------+--------'
 
 //                                 .--------------------------------------------.
