a92124282b7c7f23d55ace720049ff18723b8887
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2023-04-09 22:23:59 -0300

    Make RSE/LWR not stop caps words

--- users/rhruiz/rhruiz.c
+++ users/rhruiz/rhruiz.c
@@ -52,6 +52,28 @@ void next_default_layer(rhruiz_runtime_state *state) {
     default_layer_set(1 << (layer & mask));
 }
 
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+        case KC_MINS:
+            add_weak_mods(MOD_BIT(KC_LSFT)); // Apply shift to next key.
+            return true;
+
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_1 ... KC_0:
+        case MO_LWR:
+        case MO_RSE:
+        case KC_BSPC:
+        case KC_DEL:
+        case KC_UNDS:
+            return true;
+
+        default:
+            return false; // Deactivate Caps Word.
+    }
+}
+
 #ifdef SPLIT_KEYBOARD
 void sync_runtime_state_handler(uint8_t in_buflen, const void* in_data, uint8_t out_buflen, void* out_data) {
     const master_to_slave_t *mstate = (const master_to_slave_t*)in_data;
