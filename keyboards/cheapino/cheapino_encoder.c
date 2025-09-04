80e50608789938b27e524a2fd8bfad69a6eae255
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2025-09-04 17:02:36 -0300

    allow using standard qmk features like encoder map

--- keyboards/cheapino/cheapino_encoder.c
+++ keyboards/cheapino/cheapino_encoder.c
@@ -8,12 +8,15 @@
 #define ENC_B_COL 4
 #define ENC_BUTTON_COL 0
 
-static bool colABPressed   = false;
-// static bool encoderPressed = false;
+#ifndef ENCODER_DIRECTION_FLIP
+#    define ENCODER_CLOCKWISE true
+#    define ENCODER_COUNTER_CLOCKWISE false
+#else
+#    define ENCODER_CLOCKWISE false
+#    define ENCODER_COUNTER_CLOCKWISE true
+#endif
 
-void clicked(void) {
-    tap_code(KC_MPLY);
-}
+static bool colABPressed = false;
 
 bool encoder_update_kb(uint8_t index, bool clockwise) {
 #ifdef ENCODER_ENABLE
@@ -22,33 +25,11 @@ bool encoder_update_kb(uint8_t index, bool clockwise) {
     }
 #endif
 
-    if (index == 0) {
-        if (IS_LAYER_ON(6)) {
-            tap_code(clockwise ? KC_VOLU : KC_VOLD);
-        } else if (IS_LAYER_ON(3)) {
-            tap_code16(clockwise ? LCTL(KC_TAB) : LCTL(LSFT(KC_TAB)));
-        } else if (IS_LAYER_ON(5)) {
-            tap_code16(clockwise ? LGUI(KC_Y) : LGUI(KC_Z));
-        } else {
-            tap_code16(clockwise ? KC_PGDN : KC_PGUP);
-        }
-    }
-
     return true;
 }
 
 void fix_encoder_action(matrix_row_t current_matrix[]) {
     matrix_row_t encoder_row = current_matrix[ENC_ROW];
-
-    // if (encoder_row & (COL_SHIFTER << ENC_BUTTON_COL)) {
-    //     encoderPressed = true;
-    // } else {
-    //     // Only trigger click on release
-    //     if (encoderPressed) {
-    //         encoderPressed = false;
-    //         clicked();
-    //     }
-    // }
 
     // Check which way the encoder is turned:
     bool colA = encoder_row & (COL_SHIFTER << ENC_A_COL);
@@ -60,14 +41,13 @@ void fix_encoder_action(matrix_row_t current_matrix[]) {
         if (colABPressed) {
             // A+B followed by A means clockwise
             colABPressed = false;
-            encoder_update_kb(0, true);
+            encoder_queue_event(0, ENCODER_CLOCKWISE);
         }
     } else if (colB) {
         if (colABPressed) {
             // A+B followed by B means counter-clockwise
             colABPressed = false;
-            encoder_update_kb(0, false);
+            encoder_queue_event(0, ENCODER_COUNTER_CLOCKWISE);
         }
     }
-    // current_matrix[ENC_ROW] = 0;
 }
