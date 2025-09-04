694a03c2cf0461371f6467dab65128bd07eb008d
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2025-09-04 17:02:11 -0300

    bring cheapino over, make encoders work natively

---
+++ keyboards/cheapino/config.h
@@ -0,0 +1,40 @@
+// Copyright 2023 Thomas Haukland (@tompi)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+#define WS2812_PIO_USE_PIO1 // Force the usage of PIO1 peripheral, by default the WS2812 implementation uses the PIO0 peripheral
+//#define WS2812_TRST_US 80
+#define WS2812_BYTE_ORDER WS2812_BYTE_ORDER_GRB
+
+#define MAX_DEFERRED_EXECUTORS 32
+
+// #define DEBUG_MATRIX_SCAN_RATE
+
+#define RGBLIGHT_DEFAULT_HUE 128 // Sets the default hue value, if none has been set
+#define RGBLIGHT_DEFAULT_SAT 128 // Sets the default saturation value, if none has been set
+#define RGBLIGHT_DEFAULT_VAL 32 // Sets the default brightness value, if none has been set
+
+
+#ifdef ENCODER_ENABLE
+#define ENCODERS_PAD_A { GP12 }
+#define ENCODERS_PAD_B { GP13 }
+#define NUM_ENCODERS 1
+#define ENCODER_RESOLUTION 1
+#endif
---
+++ keyboards/cheapino/rules.mk
@@ -0,0 +1,6 @@
+CAPS_WORD_ENABLE = yes
+CUSTOM_MATRIX = lite
+DEFERRED_EXEC_ENABLE = yes
+SRC += cheapino_encoder.c
+SRC += ghosting.c
+SRC += matrix.c
